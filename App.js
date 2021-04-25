0d9c1b23ce3b798504b57d390ac0bb8ff2229035
randeview <nurlantukenbayev@gmail.com>
2021-04-25 23:36:46 +0600

    Task todo finish

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import RootComponent from './navigation/RootComponent';
+
+// Nurlan Tukenbayev 20MD0239
+export default class App extends React.Component {
+  render() {
+    return <RootComponent />;
+  }
+}
